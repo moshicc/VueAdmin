d502aa4299d4ef34014a648f189ae5687f04dec0
张聪聪 <1316435855@qq.com>
2021-05-08 19:14:50 +0800

    1.添加测试Controller 进行测试 2.添加结果返回封装类

---
+++ src/main/java/com/zcc/common/lang/Result.java
@@ -0,0 +1,46 @@
+package com.zcc.common.lang;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author zcc
+ * @ClassName Result
+ * @description
+ * @date 2021/5/8 19:02
+ * @Version 1.0
+ */
+@Data
+public class Result implements Serializable {
+
+    private int code; // 200是正常，非200表示异常
+    private String msg;
+    private Object data;
+
+    public static Result succ(Object data) {
+        return succ(200, "操作成功", data);
+    }
+    public static Result succ(int code, String msg, Object data) {
+        Result result = new Result();
+        result.setCode(code);
+        result.setMsg(msg);
+        result.setData(data);
+        return result;
+    }
+    public static Result fail(String msg) {
+        return fail(400, msg, null);
+    }
+    public static Result fail(String msg, Object data) {
+        return fail(400, msg, data);
+    }
+    public static Result fail(int code, String msg, Object data) {
+        Result r = new Result();
+        r.setCode(code);
+        r.setMsg(msg);
+        r.setData(data);
+        return r;
+    }
+
+
+}
---
+++ src/main/java/com/zcc/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.zcc.controller;
+
+import com.zcc.service.SysUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zcc
+ * @ClassName TestController
+ * @description
+ * @date 2021/5/8 18:47
+ * @Version 1.0
+ */
+@RestController
+public class TestController {
+
+    @Autowired
+    SysUserService sysUserService;
+
+    @GetMapping("/test")
+    public Object test() {
+        return sysUserService.list();
+    }
+}
