ed61affca7aef06a2e333d07b9a6d3c0cbe6e3fa
张聪聪 <1316435855@qq.com>
2021-05-08 19:27:19 +0800

    1.添加全局异常处理类

--- src/main/java/com/zcc/controller/TestController.java
+++ src/main/java/com/zcc/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.zcc.controller;
 
+import com.zcc.common.lang.Result;
 import com.zcc.service.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,6 @@ public class TestController {
 
     @GetMapping("/test")
     public Object test() {
-        return sysUserService.list();
+        return Result.succ(sysUserService.list());
     }
 }
